d38cdb4e0dce89fbf286d3109619ebb8976a8997
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2023-01-12 13:24:04 -0500

    application for uwsgi

--- verify_wikipedia/wsgi.py
+++ verify_wikipedia/wsgi.py
@@ -265,10 +265,7 @@ def test_model():
     logging.info("Model working as expected.")
 
 load_model()
+application = app
 
 if __name__ == '__main__':
     app.run()
-else:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
